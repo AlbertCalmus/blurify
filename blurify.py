929fe4b0bd4b08e2a22fc3914a499d478f070c21
AlbertCalmus <avequin.da@gmail.com>
2020-10-13 16:12:12 +0200

    Add python file

---
+++ blurify.py
@@ -0,0 +1,18 @@
+import sys
+import imageio
+import numpy as np
+from PIL import Image, ImageFilter
+
+
+def generate_video_from_img(img_path):
+  img = Image.open(img_path)
+  writer = imageio.get_writer('videos/{}.mp4'.format(np.abs(hash(img_path))), format='FFMPEG', mode='I', fps=10)
+    
+  for i in range(100, -1, -1):
+    new_frame = img.filter(ImageFilter.GaussianBlur(i))
+    writer.append_data(np.asarray(new_frame))
+  writer.close()
+  print("  > Video successfully created!")
+
+if __name__ == '__main__':
+  generate_video_from_img(sys.argv[-1])
